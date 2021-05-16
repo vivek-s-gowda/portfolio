2d3721e1a43ff19a01a5eae4563a405d1388fed9
Vivek S <vivek.s@atonarp.com>
2021-05-16 22:35:23 +0530

    image added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+  introduction: boolean = false;
+  projects: boolean = false;
+  contact: boolean = false;
+
+  onContentClick(content: string) {
+    switch (content) {
+      case 'Introduction':
+        this.introduction = true;
+        this.projects = false;
+        this.contact = false;
+        break;
+      case 'Projects':
+        this.introduction = false;
+        this.projects = true;
+        this.contact = false;
+        break;
+      case 'Contact':
+        this.introduction = false;
+        this.projects = false;
+        this.contact = true;
+        break;
+    }
+  }
 }
