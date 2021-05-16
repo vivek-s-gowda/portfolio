1bcadb04c82152cfd4b5039bf878ff3530659b61
Vivek S <vivek.s@atonarp.com>
2021-05-16 23:03:58 +0530

    font updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,23 +10,32 @@ export class AppComponent {
   introduction: boolean = false;
   projects: boolean = false;
   contact: boolean = false;
-
+  content: boolean = true;
   onContentClick(content: string) {
     switch (content) {
       case 'Introduction':
         this.introduction = true;
         this.projects = false;
         this.contact = false;
+        this.content = false;
         break;
       case 'Projects':
         this.introduction = false;
         this.projects = true;
         this.contact = false;
+        this.content = false;
         break;
       case 'Contact':
         this.introduction = false;
         this.projects = false;
         this.contact = true;
+        this.content = false;
+        break;
+      case 'Content':
+        this.introduction = false;
+        this.projects = false;
+        this.contact = false;
+        this.content = true;
         break;
     }
   }
