dcf1856f9de4db4964377d3ca26eb41063d3cb02
Vivek S <vivek.s@atonarp.com>
2021-05-17 13:25:14 +0530

    new components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
