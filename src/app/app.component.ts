4e645d920d1d89906b6b37bcb546e0512b156678
Vivek S <vivek.s@atonarp.com>
2021-05-17 16:31:55 +0530

    touch events added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,4 +39,8 @@ export class AppComponent {
         break;
     }
   }
+
+  say(message) {
+    alert(message);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import * as Hammer from 'hammerjs';
+import {
+  HammerModule,
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG,
+} from '@angular/platform-browser';
+
+@Injectable()
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    swipe: { direction: Hammer.DIRECTION_ALL },
+  };
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
     ProjectsComponent,
-    ContactComponent
+    ContactComponent,
   ],
-  imports: [
-    BrowserModule
+  imports: [BrowserModule, HammerModule],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    },
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
