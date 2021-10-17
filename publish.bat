f87367761fc5a704f7f9046c72c24980763c24bd
Vivek S <sgowdavivek@gmail.com>
2021-10-17 11:00:30 +0530

    changes from script

--- publish.bat
+++ publish.bat
@@ -1,5 +1,11 @@
+echo "Adding Local changes ....."
 git add .
+echo "Adding Local changes completed ....."
 git commit -m "changes from script"
+echo "Added Commit message ....."
+echo "Pushing your changes to git ...."
 git push 
+echo "Deploying code to github pages ....."
 ng build --prod --base-href "https://vivek-s-gowda.github.io/portfolio/"
 npx angular-cli-ghpages --dir=dist/portfolio
+echo "Your code successfully deployed :)"
